62088f1785632f27cdbec5bdb609149387cd5f67
paulina0813 <pauremis08@gmail.com>
2024-02-19 20:23:36 -0600

    Debugs and virtual environments

---
+++ 021924/debug/021924.001debug.py
@@ -0,0 +1,8 @@
+# Manual debug
+def double(number):
+    print("Number:", number)
+    result =  number * 2
+    print("Result", result)
+    return result
+
+print(double(2))
---
+++ 021924/debug/021924.002.debug_module.py
@@ -0,0 +1,11 @@
+import pdb
+
+def double(x):
+    return x*2
+
+
+val = 3
+
+print(f"{val} * 2 is {double(val)}")
+
+#Run python3 -m pdb 021924.002.debug_module.py in terminal
